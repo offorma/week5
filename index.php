fa9dab8ad21c0764fadc05fd00e73b0abd71eb03
offorma <offorma@gmail.com>
2016-03-14 10:42:14 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+
+
+<DOCTYPE !html>
+    <html>
+        <head>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+        </head>
+        <body>
+        <form action="">
+            <div class="form-group">
+                <label for="InputEmail1">Email address</label>
+                <input type="email" name="email" class="form-control" id="InputEmail1" placeholder="Email">
+            </div>
+            <div class="form-group">
+                <label for="InputPassword1">Password</label>
+                <input type="password" name="password" class="form-control" id="InputPassword1" placeholder="Password">
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+        </body>
+    </html>
+</DOCTYPE>
+
+
+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1514414
+ * Date: 14/03/2016
+ * Time: 10:29
+ */
---
+++ server.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1514414
+ * Date: 14/03/2016
+ * Time: 10:22
+ */
+define('DB_SERVER','ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_USERNAME','bccb84f33826f0');
+define('DB_PASSWORD','7b0e4bef');
+define('DB_DATABASE','Nonso1514414');
+
+$DB = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+?>
