a832620e6fe7b678d9a2b5aa618bf474dec4e7ed
offorma <offorma@gmail.com>
2016-03-15 09:50:26 +0000

    1

--- login.php
+++ login.php
@@ -9,18 +9,18 @@ include("server.php");
 
 if(empty($_POST["username"]) || empty($_POST["password"])){
     echo "Both fields are required.";
-}else{
+}
+else{
     $username = $_POST["username"];
     $password = $_POST["password"];
 
     $sql="SELECT uid FROM users WHERE username='$username' and password='$password'";
     $result=mysqli_query($db,$sql);
 
-    if(mysqli_num_rows($result) == 1)
-    {
+    if(mysqli_num_rows($result) == 1) {
         header("location: index.php"); // Redirecting To another Page
-    }else
-    {
+    }
+    else {
         echo "Incorrect username or password.";
     }
 
--- server.php
+++ server.php
@@ -12,4 +12,11 @@ define('DB_DATABASE','Nonso1514414');
 
 $DB = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
+if (!$DB) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
 ?>
